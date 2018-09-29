23f15dab2d87daf18b5f267e7f4cb22b6b72f225
Kumar Pratik <kumar.pratik73@yahoo.com>
2018-09-29 17:16:29 +0200

    Removed Null Point Exceptions

--- Simple_EA.java
+++ Simple_EA.java
@@ -4,12 +4,10 @@ import java.util.Collections;
 import java.util.List;
 import org.vu.contest.ContestEvaluation;
 
-import com.sun.corba.se.impl.javax.rmi.CORBA.Util;
-
 public class Simple_EA {
 	// Uncorrelated self-adaptive mutation parameter
-	public double tau = 5*Math.sqrt(1/20.0);
-	public double tauPrime = 5*Math.pow(1/40.0, 0.25);
+	public double tau = Math.sqrt(1/20.0);
+	public double tauPrime = Math.pow(1/40.0, 0.25);
 	// threshold for step sizes for self-adaptive mutation
 	public double epsilon = 0.3;
 	// Constants used for linear rank based parent selection
@@ -42,7 +40,7 @@ public class Simple_EA {
 
     public void evolve() {
     //This only works with even populationsize!
-        List<Individual> parents = chooseParents((int)(populationsize*2/3.0));
+        List<Individual> parents = chooseParents((int)(populationsize*2/3));
         List<Individual> children = createChildren(parents, populationsize);
         List<Individual> newpop = new ArrayList<Individual>();
         newpop.addAll(children);
@@ -50,8 +48,8 @@ public class Simple_EA {
     }
     
 	public static double probParent(int rank) {
-		return (1-Math.exp(-1*rank))/(double)c3;
-//		return (c1 + c2*rank);
+//		return (1-Math.exp(-1*rank))/(double)c3;
+		return (c1 + c2*rank);
 	}
 	
 	public static double sumProbParent(int index, List<Integer> popList) {
@@ -102,13 +100,13 @@ public class Simple_EA {
 
     public List<Individual> createChildren(List<Individual> parents, int size) {
         List<Individual> children = new ArrayList<Individual>(size);
-        for(int i=0;i<size;i=i+2) {
-            Individual p1 = parents.get(i);
-            Individual p2 = parents.get(i+1);
+        for(int x=0;x<parents.size();x=x+2) {
+            Individual p1 = parents.get(x);
+            Individual p2 = parents.get(x+1);
             List<Individual> childList = crossover(p1, p2);
-            children.add(mutate(childList.get(0)));
-            children.add(mutate(childList.get(1)));
-            children.add(mutate(childList.get(2)));
+            children.add(childList.get(0));
+            children.add(childList.get(1));
+            children.add(childList.get(2));
         }
         return children;
     }
@@ -121,7 +119,12 @@ public class Simple_EA {
         double[] child3genes = new double[10];
         double[] child3stepsize = new double[10];
         double alpha = Math.random();
-        double fit_ratio = p1.getFitness()/(p1.getFitness()+p2.getFitness());        
+        double fit_ratio = 0.5;
+        if(p1.getFitness()*p2.getFitness() != 0) {
+        	fit_ratio = p1.getFitness()/(p1.getFitness()+p2.getFitness());  
+        } else {
+        	fit_ratio = 0.5;
+        }
         for (int i=0;i<10; i++) {
         	child1genes[i] = alpha*p1.getGenes()[i] + (1-alpha)*p2.getGenes()[i];
         	child1stepsize[i] = alpha*p1.getStepsize()[i] + (1-alpha)*p2.getStepsize()[i];
@@ -130,18 +133,19 @@ public class Simple_EA {
         	child3genes[i] = fit_ratio*p1.getGenes()[i] + (1-fit_ratio)*p2.getGenes()[i];
         	child3stepsize[i] = fit_ratio*p1.getStepsize()[i] + (1-fit_ratio)*p2.getStepsize()[i];
         }
-        List<Individual>children = new ArrayList<>(3);
-        children.add(new Individual(this.evaluation, child1genes, child1stepsize ));
-        children.add(new Individual(this.evaluation, child2genes, child2stepsize ));
-        children.add(new Individual(this.evaluation, child3genes, child3stepsize ));
+        List<Individual>children = new ArrayList<>();
+        List<double[]> genotypes = new ArrayList<>();
+        genotypes = mutate(child3genes, child3stepsize);
+        children.add(new Individual(this.evaluation, genotypes.get(0), genotypes.get(1) ));
+        genotypes = mutate(child2genes, child2stepsize);
+        children.add(new Individual(this.evaluation, genotypes.get(0), genotypes.get(1) ));
+        genotypes = mutate(child1genes, child1stepsize);
+        children.add(new Individual(this.evaluation, genotypes.get(0), genotypes.get(1) ));
         return children;
     }
 
-    public Individual mutate(Individual c1) {
-    	double[] genes = new double [10];
-    	double[] stepsizes = new double[10];
-    	genes = c1.getGenes();
-    	stepsizes = c1.getStepsize();
+    public List<double[]> mutate(double[] genes, double[] stepsizes) {
+    	List<double[]> geneStepList = new ArrayList<>();
     	double random1 = r.nextGaussian();
         for (int i = 0; i< genes.length; i++) {
         	stepsizes[i] = stepsizes[i]*Math.exp(this.tauPrime*random1 + this.tau*r2.nextGaussian());  
@@ -150,6 +154,8 @@ public class Simple_EA {
             }
         	genes[i] = genes[i] + stepsizes[i]*r2.nextGaussian();
         }
-        return new Individual(this.evaluation, genes, stepsizes);
+        geneStepList.add(genes);
+        geneStepList.add(stepsizes);
+        return (geneStepList);
     }
 }
