9bf652ccde5c1e742360f21290bb85bc6a4a95be
Alex Malyutin <malutin.alex@gmail.com>
2018-10-15 22:51:30 +0200

    ..

--- SubmitHere/wtf.sh
+++ SubmitHere/wtf.sh
@@ -1,8 +1,12 @@
-var1='i'
-for popSize in 50 60
+#!/usr/bin/env bash
+for popSize in 20 40
 do
     for j in {1..4}
     do
-        java -DpopSize=$popSize -Dmethod="baseline" -DupdSize=1 -DselectionPressure=1.8 -jar testrun.jar > "./out/BentCigar/storeresults$popSize$var1$j.txt" -submission=player52 -evaluation=BentCigarFunction -seed=2
+        java -DpopSize=$popSize -Dmethod="crowding" -DupdSize=1 -DselectionPressure=1.8 \
+        -jar testrun.jar > "./out/BentCigar/crowding/Size_$popSize_$j.txt" \
+         -submission=player52 \
+         -evaluation=BentCigarFunction \
+         -seed=2
     done
 done
