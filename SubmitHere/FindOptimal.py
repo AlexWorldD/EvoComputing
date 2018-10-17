3f62fd0336504c177ad8e297cf39c190c518d2d1
Alex Malyutin <malutin.alex@gmail.com>
2018-10-17 14:13:16 +0200

    Updated python script, was a problem with str(float)...

--- SubmitHere/FindOptimal.py
+++ SubmitHere/FindOptimal.py
@@ -1,13 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-popSize = np.array([20, 50, 80, 150], int)
-sigma = np.array([0.001, 0.05, 0.15, 0.3, 0.5, 0.8])
-selPressure = np.array([1.0, 1.8])
-eps = np.array([0.0, 0.01, 0.1])
+
+# Schaffers
+# popSize = np.array([20, 50, 80, 150], int)
+# sigma = np.array([0.001, 0.05, 0.15, 0.3, 0.5, 0.8])
+# selPressure = np.array([1.0, 1.8])
+# eps = np.array([0.0, 0.01, 0.1])
+# Katsuura
+popSize = np.array([200, 300, 400, 500], int)
+sigma = np.array([0.0001, 0.0005, 0.001], float)
+selPressure = np.array([1.85, 1.95])
+eps = np.array(['0.00001', '0.00005'])
 popFit = []
 simltnSize = 5
-dir = "out/Schaffers/crowding/"
+# dir = "out/Schaffers/crowding/"
+dir = "out/Katsuura/crowding/"
 format = ".txt"
 combinations = [(psize, s, sp, _eps) for _eps in eps for psize in popSize for s in sigma for sp in selPressure]
 print(combinations)
@@ -15,7 +23,7 @@ score = dict()
 for psize, s, sp, _eps in combinations:
     som = 0
     for j in range(1, simltnSize + 1):
-        dirctry = dir + "Size_" + str(psize) + "_Sigma_" + str(s) + "_SelPressure_" + str(sp) + "_Eps_" +str(_eps)+"_" + str(j) + format
+        dirctry = dir + "Size_" + str(psize) + "_Sigma_" + str(s) + "_SelPressure_" + str(sp) + "_Eps_" +_eps+"_" + str(j) + format
         lines = np.loadtxt(dirctry, comments="R", delimiter=" ", unpack=False, usecols=1)
         som += lines
     score[(psize, s, sp, _eps)] = som / simltnSize
