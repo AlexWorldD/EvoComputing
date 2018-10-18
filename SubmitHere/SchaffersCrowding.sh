adda9685616baea7ed6ced79ceb1d87e3dd174d9
Alex Malyutin <malutin.alex@gmail.com>
2018-10-18 21:50:16 +0200

    Schaffers for Lotta

---
+++ SubmitHere/SchaffersCrowding.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+for popSize in 50
+do
+    for sigma in  0.8
+    do
+        for selPressure in 1.1
+        do
+            for eps in 0.0
+            do
+                for j in {1..100}
+                do
+                    java -DpopSize=$popSize -Dmethod="crowding" -Dsigma=$sigma -DupdSize=1 -DselectionPressure=$selPressure -Deps=$eps \
+                    -jar testrun.jar > "./out/Schaffers/crowding/final/Size_"$popSize"_Sigma_"$sigma"_SelPressure_"$selPressure"_Eps_"$eps"_"$j".txt" \
+                     -submission=player52 \
+                     -evaluation=SchaffersEvaluation \
+                     -seed=2
+                done
+                echo "Size_"$popSize"_Sigma_"$sigma"_SelPressure_"$selPressure"_Eps_"$eps
+            done
+         done
+    done
+done
